6827a1c313beff7d8c1ad672758c66377b7dafb6
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-10-24 19:30:13 +0200

    add views from course repository

---
+++ src/test/java/com/dba/JAVA_job_app/JavaJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dba.JAVA_job_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
