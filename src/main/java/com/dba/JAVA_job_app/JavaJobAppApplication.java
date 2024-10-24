29ec0032fb6b90738cb8178de80bfdfa57b631d1
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-10-24 19:29:16 +0200

    add basic project structure

---
+++ src/main/java/com/dba/JAVA_job_app/JavaJobAppApplication.java
@@ -0,0 +1,13 @@
+package com.dba.JAVA_job_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJobAppApplication.class, args);
+	}
+
+}
