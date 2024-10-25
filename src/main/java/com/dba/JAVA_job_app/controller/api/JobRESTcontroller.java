3b1035fc4215ebd4a1d3fdcba9a42ab35e7bbcdb
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-10-25 17:47:45 +0200

    add addJob api

--- src/main/java/com/dba/JAVA_job_app/controller/api/JobRESTcontroller.java
+++ src/main/java/com/dba/JAVA_job_app/controller/api/JobRESTcontroller.java
@@ -9,6 +9,8 @@ import com.dba.JAVA_job_app.service.JobService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class JobRESTcontroller {
@@ -25,4 +27,11 @@ public class JobRESTcontroller {
     public JobPost getJobAPI(@PathVariable("postId") int postId) {
         return service.getJob(postId);
     }
+
+    @PostMapping("jobPost")
+    public JobPost addJobAPI(@RequestBody JobPost jobPost) {
+        service.addJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
 }
