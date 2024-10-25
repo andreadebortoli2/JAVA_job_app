0a81d2a90149fe46e5e5b43e8fb6447ec5496015
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-10-25 17:26:49 +0200

    add get single job post api

--- src/main/java/com/dba/JAVA_job_app/controller/api/JobRESTcontroller.java
+++ src/main/java/com/dba/JAVA_job_app/controller/api/JobRESTcontroller.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.dba.JAVA_job_app.model.JobPost;
 import com.dba.JAVA_job_app.service.JobService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +17,12 @@ public class JobRESTcontroller {
     private JobService service;
 
     @GetMapping("jobPosts")
-    public List<JobPost> allJobsAPI() {
+    public List<JobPost> getAllJobsAPI() {
         return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJobAPI(@PathVariable("postId") int postId) {
+        return service.getJob(postId);
     }
 }
