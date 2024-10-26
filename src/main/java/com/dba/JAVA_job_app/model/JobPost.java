91b945ab1d7d0ddbbd39d52f85a4c62ad7ba03c1
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-10-26 16:11:54 +0200

    edit model with auto incremental id and image fields

--- src/main/java/com/dba/JAVA_job_app/model/JobPost.java
+++ src/main/java/com/dba/JAVA_job_app/model/JobPost.java
@@ -1,27 +1,41 @@
 package com.dba.JAVA_job_app.model;
 
+import java.util.Date;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+// import org.springframework.stereotype.Component;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// @Component
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
-@Entity
 public class JobPost {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-generate incremental index number
     private int postId;
     private String postProfile;
     private String postDesc;
     private int reqExperience;
     private List<String> postTechStack;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") // define the format of the date i Json
+    private Date postDate;
+    // add image fields
+    private String imageName;
+    private String imageType;
+    @Lob // define as large object
+    private byte[] imageData;
 
 }
