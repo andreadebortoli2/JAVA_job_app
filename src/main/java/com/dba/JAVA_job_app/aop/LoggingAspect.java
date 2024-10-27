530936f066e20ae21cbdba5d9426b6313a0868fb
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-10-27 13:55:15 +0100

    add aop logger afterp

--- src/main/java/com/dba/JAVA_job_app/aop/LoggingAspect.java
+++ src/main/java/com/dba/JAVA_job_app/aop/LoggingAspect.java
@@ -1,6 +1,9 @@
 package com.dba.JAVA_job_app.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -20,7 +23,22 @@ public class LoggingAspect {
     // @Before("execution(* com.dba.JAVA_job_app.service.JobService.*(..))")
     // before "all methods" in service-package.JobService.class
     @Before("execution(* com.dba.JAVA_job_app.service.JobService.getAllJobs(..))")
-    public void logMethodCallpp(JoinPoint jp) {
+    public void logMethodCall(JoinPoint jp) {
         LOGGER.info("Method called: " + jp.getSignature().getName());
+    }
+
+    @After("execution(* com.dba.JAVA_job_app.service.JobService.getAllJobs(..))")
+    public void logMethodExcecuted(JoinPoint jp) {
+        LOGGER.info("Method executed: " + jp.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.dba.JAVA_job_app.service.JobService.addOrUpdateJob(..))")
+    public void logMethodExcecutedSuccessfully(JoinPoint jp) {
+        LOGGER.info("Method executed successfully: " + jp.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.dba.JAVA_job_app.service.JobService.addOrUpdateJob(..))")
+    public void logMethodCrash(JoinPoint jp) {
+        LOGGER.info("Method has some issues: " + jp.getSignature().getName());
     }
 }
